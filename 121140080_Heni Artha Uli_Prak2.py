280bc2b4e5e54cd971a5cbc99533e91c454a186a
heniturnip <127072361+heniturnip@users.noreply.github.com>
2023-03-13 14:42:34 +0700

    Tugas2

---
+++ 121140080_Heni Artha Uli_Prak2.py
@@ -0,0 +1,14 @@
+class Mahasiswa:
+    def _init_(self, nama, nim, kls, sks):
+        self.nama = nama
+        self.nim = nim
+        self.kls = kls
+        self.sks = sks
+    def tampil_mahasiswa(self):
+        print("Heni Artha Uli Br Turnip")
+        print("121140080")
+        print("RB")
+        print("20")
+
+Mahasiswa = Mahasiswa()
+Mahasiswa.tampil_mahasiswa()
